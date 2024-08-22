1f657696f84f3ae43192b5853083ee96a48e6deb
Isyana Putri Indriana <isyanap@gmail.com>
2024-08-22 23:22:12 +0700

    mengedit penjelasan WelcomeJava

--- pertemuan_1/WelcomeJava.java
+++ pertemuan_1/WelcomeJava.java
@@ -19,7 +19,7 @@ public class WelcomeJava {
       String greeting = "Welcome to Core Java!"; //variabel greeting yang bertipe data string yang berisi "Welcome to Core Java"
       System.out.println(greeting); //isi dari variabel greeting akan ditampilkan ke layar dan membuat barisan baru
       for (int i = 0; i < greeting.length(); i++) //jika i kurang dari panjang isi dari variabel greeting 
-        System.out.print("="); //maka akan menambahkan "=" sampai akhir panjang variabel greeting.
+        System.out.print("="); //maka akan menambahkan atau lopping "=" sampai akhir panjang isi dari variabel greeting.
         System.out.println(); 
     }
     
