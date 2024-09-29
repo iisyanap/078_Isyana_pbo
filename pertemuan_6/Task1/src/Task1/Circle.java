32309b55672982c7e70e2ac916a32c2bca2699d4
Isyana Putri Indriana <isyanap@gmail.com>
2024-09-29 16:08:30 +0700

    update pertemuan ke 6

--- pertemuan_6/Task1/src/Task1/Circle.java
+++ pertemuan_6/Task1/src/Task1/Circle.java
@@ -1,22 +1,22 @@
 package Task1;
+class Circle { 
 
-public class Circle {
     private double radius;
     private String color;
-
-    public Circle() { 
-        this.radius = 1.0;
-        this.color = "merah";
+    
+    public Circle() {
+    radius = 1.0;
+    color = "red";
+    }
+   
+    public Circle(double radius) {
+        this.radius = radius;
+        this.color = "red";
     }
 
-    public Circle(double r) { 
-        this.radius = r;
-        this.color = "merah";
-    }
-
-    public Circle(double r, String c) { 
-        this.radius = r;
-        this.color = c;
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
     }
 
     public double getRadius() {
@@ -36,12 +36,10 @@ public class Circle {
     }
 
     public double getArea() {
-        return radius * radius * Math.PI;
+        return radius * radius * Math.PI; 
     }
 
     public String toString() {
         return "Circle[radius=" + radius + ", color=" + color + "]";
     }
 }
-
-
